52eacfbac354d75f391f088dc4e57b3a06c704d9
Gabriel <gbiren@ucla.edu>
2019-03-30 16:55:50 -0700

    Navbar and Home Page Updates

--- web-2/smart-can/src/components2/Smart-Can-Dash/DashBoard.jsx
+++ web-2/smart-can/src/components2/Smart-Can-Dash/DashBoard.jsx
@@ -1,16 +1,53 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom'
+import { firestoreConnect, getVal } from 'react-redux-firebase'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { Container, Spinner} from 'reactstrap';
 
 class Home extends Component {
   render() {
-    const { auth } = this.props;
-    if (!auth.uid) return <Redirect to='/login' />
+    const { stats } = this.props;
+    const placeholder = [5]
     return (
-        <div className="Account">
-            <h1 className="text-center"> Welcome to Smart Can ? </h1>
-        </div>
+        <Container className="Account text-center">
+            <br/>
+            <i class="fas fa-dumpster fa-10x text-center"></i>
+            <h1 className="text-center display-4"> Welcome to Smart Can </h1>
+            <p className='lead text-center'>Digital Waste Sorting & Analytics</p>
+            <br/>
+            <h3>Collection Stats<br/>
+            {stats && stats.map(doc => {
+              return (
+                <div>
+                  <small class="text-muted">{doc.recycleCount} Recyclables</small><br/>
+                  <small class='text-muted'>{doc.trashCount} Non-Recyclables</small>
+                </div>
+                )
+            })}
+            {!stats && placeholder.map(empt => {
+              return (
+                <div>
+                  <Spinner style={{ width: '2rem', height: '2rem' }} />
+                </div>
+              )
+            })}
+            </h3>
+        </Container>
     );
   }
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+  console.log(state);
+  return {
+      stats: state.firestore.ordered.global
+  }
+}
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([
+    { collection: 'global', limit: 1}
+  ])
+)(Home)
--- web-2/smart-can/src/components2/Smart-Can-Nav/Navigation.jsx
+++ web-2/smart-can/src/components2/Smart-Can-Nav/Navigation.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Navbar, NavbarBrand } from 'reactstrap';
 import SignedOutLinks from './Smart-Can-SignedOutLinks';
+import './navbar.css'
 
 const Nav = (props) => {
     return (
         <div>
-            <Navbar className="nsbe-menu" color="light" opacity="0.2">
+            <Navbar className="transparent" color="faded">
             <NavbarBrand className="wText" href="/"> 
-            <i class="fas fa-dumpster-fire"></i>
+            <i class="fas fa-dumpster-fire white"></i>
               <b> Smart Can </b>
             </NavbarBrand>
             <SignedOutLinks></SignedOutLinks>
