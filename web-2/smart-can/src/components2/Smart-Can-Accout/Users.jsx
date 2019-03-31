9b539cc3afbb456b4ee35320a888afa3329d3b44
DonX86 <donovanma1@ucla.edu>
2019-03-31 06:07:13 -0700

    User pg

--- web-2/smart-can/src/components2/Smart-Can-Accout/Users.jsx
+++ web-2/smart-can/src/components2/Smart-Can-Accout/Users.jsx
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
+import { Card, CardTitle, CardText, Row, Col } from 'reactstrap';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 class Users extends Component {
-  render() {
+    render() {
+    const { profile, auth } = this.props;
+    if (!auth.uid) return <Redirect to='/login' />
     return (
-        <div className="Account">
-            <h1> Hello world</h1>
+        <div className="Account container">
+          <Card body color="dark">
+            <CardTitle><h1 className="text-center display-4">Stats</h1></CardTitle>
+            <CardText className="text-center"><b>User:</b> {profile.firstName}</CardText>
+            <Row>
+              <Col className="text-center">
+                <i class="fas fa-trash fa-3x"></i>
+                <CardText><h1 className="text-center">23</h1></CardText>
+              </Col>
+              <Col className="text-center">
+                <i class="fas fa-recycle fa-3x"></i>
+                <CardText><h1 className="text-center">50</h1></CardText>
+              </Col>
+             
+            </Row>
+          </Card>
         </div>
     );
   }
 }
 
-export default Users;
+const mapStateToProps = (state) => {
+  return {
+      auth: state.firebase.auth,
+      profile: state.firebase.profile
+  }
+}
+
+export default connect(mapStateToProps)(Users)
