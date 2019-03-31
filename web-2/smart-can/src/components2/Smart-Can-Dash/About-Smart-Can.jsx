23f938172d242d0c7668ea909b1b018e9f16dfdd
Gabriel <gbiren@ucla.edu>
2019-03-31 05:22:21 -0700

    Wrote About page

--- web-2/smart-can/src/components2/Smart-Can-Dash/About-Smart-Can.jsx
+++ web-2/smart-can/src/components2/Smart-Can-Dash/About-Smart-Can.jsx
@@ -1,29 +1,38 @@
 import React, { Component } from 'react';
-import { Jumbotron } from 'reactstrap';
-import { Card, CardImg, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+import { Container, Media, Row, Col } from 'reactstrap';
 import LA from '../../assets/LosAngeles.jpg';
+import './about.css'
 
 class About extends Component {
   render() {
     return (
       <div className="about-smart-can">
         <br></br>
-        <div className="container">
-          <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
-        <CardBody>
-          <CardTitle><h1 className="text-center"> What is Smart Can ? </h1></CardTitle>
-        </CardBody>
-        <CardBody>
-          <CardText>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-              Doloribus optio molestias delectus saepe eos, corrupti animi amet 
-              doloremque culpa quis voluptatibus tenetur ipsum necessitatibus
-               excepturi vel natus perferendis esse? Suscipit.
-            </CardText>
-            <CardImg top width="100%" src={ LA } alt="Card image cap" />
-        </CardBody>
-      </Card>
-        </div>
+        <Container>
+          <h1 className="text-center"> About Smart Can </h1>
+          <p className='lead text-center'>Smart, Sustainable Waste Management </p>
+          <Row>
+            <Col sm="10" md={{ size: 8, offset: 2 }}>
+              <br/>
+              <Media center>
+                <img className='special_image' src={LA} />
+              </Media>
+              <br/>
+              <p>
+                Based out of beautiful Los Angeles, <b>Smart Can</b> is a new way to throw away. 
+                Our bins separate waste into recyclable and non-recyclable materials and reward users for their involvement. 
+                For each CRV-eligible bottle deposited, users earn points which can be spent on a variety of rewards. 
+                Don't feel like carrying your drink can back home to redeem it? Leave it with us!
+              </p>
+              <p>
+                Together we can make waste collection smart, sustainable, and rewarding to all.
+              </p>
+              
+            </Col>
+          </Row>
+         
+
+        </Container>
       </div>
     );
   }
