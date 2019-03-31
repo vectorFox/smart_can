a650a21ab95646e0e552f581fa3c4950d7d28ee7
DonX86 <donovanma1@ucla.edu>
2019-03-30 17:42:39 -0700

    User Skeleton

---
+++ web-2/smart-can/src/components2/Smart-Can-Accout/UserStats.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
+class Stats extends Component {
+  render() {
+    return (
+        <div className="Account">
+            <h1> Hello world</h1>
+        </div>
+    );
+  }
+}
+
+export default Stats;
