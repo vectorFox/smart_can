325646e3e354e870220b93239daecbba796835cb
Gabriel <gbiren@ucla.edu>
2019-03-30 19:08:00 -0700

    Scan Page works

--- web-2/smart-can/src/components2/Smart-Can-Code/Scan.jsx
+++ web-2/smart-can/src/components2/Smart-Can-Code/Scan.jsx
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { Container, Spinner} from 'reactstrap';
 
 class Scanner extends Component {
   render() {
+    const { auth, stats } = this.props;
+    console.log(auth)
     return (
         <div className="Account">
-            <h1> Hello world</h1>
+            {stats && stats.map(doc => {
+              if (doc.activeCode) {
+                return (
+                  <Container className='text-center'>
+                    <h4 className='display-4'>Code has been detected.</h4>
+                    <p className='lead'>Please place items on dock.</p>
+                  </Container>
+                )
+              }
+              else {
+                const qrURL = 'https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=' + 
+                               auth.uid;
+                return (
+                  <Container className='text-center'>
+                    <p className='lead'>Scan at any participating bin.</p>
+                    <img src={qrURL} alt="Generating QR Code"/>
+                  </Container>
+                )
+              }
+            })}
         </div>
     );
   }
 }
 
-export default Scanner;
+const mapStateToProps = (state) => {
+  console.log(state);
+  return {
+      auth: state.firebase.auth,
+      stats: state.firestore.ordered.global
+  }
+}
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([
+    { collection: 'global', limit: 1}
+  ])
+)(Scanner)
